63afa6c2dcc9a92e18d7f76eae4dcb5199257eec
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-01 13:54:02 -0500

    fix: remove not valid export.

--- src/lib/helper/index.ts
+++ src/lib/helper/index.ts
@@ -1,4 +1,3 @@
 export { errorResponse } from './errorResponse'
 export { jsonContent } from './jsonContent'
 export { jsonContentRequired } from './jsonContentRequired'
-export { jsonError } from './jsonError'
