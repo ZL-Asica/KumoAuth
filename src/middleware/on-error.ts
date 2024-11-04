3372116f016ac025ae183b26279dc3701e1ac1df
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-04 13:53:18 -0600

    fix: Catch and handle any HTTPException now

--- src/middleware/on-error.ts
+++ src/middleware/on-error.ts
@@ -1,10 +1,14 @@
 import type { Context, ErrorHandler } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 import type { HTTPResponseError } from 'hono/types'
 
 export const onError: ErrorHandler = (
   err: Error | HTTPResponseError,
   c: Context
 ) => {
-  console.error(`Error: ${err}`)
+  if (err instanceof HTTPException) {
+    // If the error is an instance of HTTPException, return the error message
+    return c.json({ error: err.message }, err.status)
+  }
   return c.json({ error: 'Internal Server Error' }, 500)
 }
