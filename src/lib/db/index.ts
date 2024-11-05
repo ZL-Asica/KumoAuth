04691be31dc9616990a58e60e1485a633ef2b4d4
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-04 20:02:36 -0600

    feat(DB): Add get current password and update password functions

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,3 +1,4 @@
 export { getPasswordRoleByUserRoleId } from './get-password-role'
 export { getUserByUserId, getUserByUsername } from './get-user'
+export { getUserPasswordByUserId, setUserPasswordByUserId } from './password'
 export { createNewUser } from './set-user'
