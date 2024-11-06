65f11d93dcb24a6f8f861ec7ef772925f895f841
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-05 19:49:18 -0600

    fix(not-found): Fix typo of F in Found

--- src/middleware/not-found.ts
+++ src/middleware/not-found.ts
@@ -1,5 +1,5 @@
 import type { Context, NotFoundHandler } from '@/types'
 
 export const notFound: NotFoundHandler = (c: Context) => {
-  return c.json({ error: `Not found - ${c.req.path}` }, 404)
+  return c.json({ error: `Not Found - ${c.req.path}` }, 404)
 }
